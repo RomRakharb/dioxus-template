f057bf757fe3b234d2d564385fb937c0930a5b3b
Rom Rakharb <121065878+RomRakharb@users.noreply.github.com>
2025-03-12 11:31:13 +0700

    Update install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -5,19 +5,25 @@ echo "---------------------"
 echo "starts install script"
 echo "---------------------"
 echo
+
 sudo apt update && sudo apt -y upgrade
 sudo apt-get install -y software-properties-common
 sudo apt update
+
 sudo add-apt-repository -y ppa:maveonair/helix-editor
 sudo apt update
 sudo apt install helix
+
 cargo install bacon
 rustup component add rust-analyzer
+npm install --save vscode-css-languageservice
 cargo install cargo-binstall
+
 cargo binstall -y dioxus-cli
 sudo apt update
 rm README.md
 dx init
+
 echo
 echo "---------------------"
 echo "install script ends"
